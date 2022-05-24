fa78c372727206cce564e24c2301e094099fffe0
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-24 16:54:04 +0600

    Home page route setup done in navbar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
     </div>
   );
