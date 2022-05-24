153f53b380960b7b35567c992b14b4c203b09b39
mdarafatyeasin <yeasinarafat54239@gmail.com>
2022-05-24 16:20:55 +0600

    Basic setup done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3D6D68",
+          secondary: "#EFF2F3",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
